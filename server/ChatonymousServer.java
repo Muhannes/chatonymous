10329547b1d3fd60d537343f6a66ab23a6e31ad7
Muhannes <haneda-3@student.ltu.se>
2017-05-19 11:43:22 +0200

    Added print to server

--- server/ChatonymousServer.java
+++ server/ChatonymousServer.java
@@ -127,6 +127,7 @@ public class ChatonymousServer {
         String read;
         try {
           while((read = in.readLine()) != null){
+            System.out.println(read);
             out.println(read);
           }
         }catch(IOException e){
